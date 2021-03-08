b21ec160eca06e90f36ebe4ab70a0c36d70197d0
Phu0903 <71036328+Phu0903@users.noreply.github.com>
2021-03-09 01:30:31 +0700

    Store.js up date

---
+++ src/Store.js
@@ -0,0 +1,41 @@
+var redux = require('redux');
+var oldState = {
+  num:["man hinh","chuot","ban phim"],
+  editstatus:true
+}
+var reducer1 = (state=oldState,action) =>{
+  switch (action.type) {
+    case "CHANGE_EDIT_STATUS":
+       return {...state,editstatus:!state.editstatus}
+      break;
+      case "ADD_NEW":
+        return {...state,num:[...state.num,action.newItem]}
+       break;
+      case "DELETE":
+        return {...state,num:state.num.filter((value,i)  => i !== action.index
+          )}
+          break;
+    default:
+      return state
+      break;
+  }
+  
+}
+
+var store1 =redux.createStore(reducer1);
+store1.subscribe(()=>{ //hàm theo dõi 
+  console.log(JSON.stringify(store1.getState()));
+})
+store1.dispatch({type:"CHANGE_EDIT_STATUS"})
+
+store1.dispatch({
+  type:"ADD_NEW",
+  newItem:"Tai Nghe"
+})
+
+
+store1.dispatch({
+  type:"DELETE",
+  index:0
+})
+export default store1
